946157b8a961cbc00b33954a47ea389f9b834810
victor <vvazquez307@gmail.com>
2023-03-13 09:24:23 -0500

    added tables

--- db/seedData.js
+++ db/seedData.js
@@ -5,11 +5,37 @@ const client = require("./client")
 async function dropTables() {
   console.log("Dropping All Tables...")
   // drop all tables, in the correct order
+  await client.query(`
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS routines;
+    DROP TABLE IF EXISTS activities;
+  `);
+ 
 }
 
 async function createTables() {
   console.log("Starting to build tables...")
   // create all tables, in the correct order
+  await client.query(`
+  CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+  );
+  CREATE TABLE routines(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    "creatorId" INTEGER REFERENCES users(id),
+    goal TEXT NOT NULL,
+    "isPublic" BOOLEAN DEFAULT true
+  );
+  CREATE TABLE activities(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL, 
+    description TEXT NOT NULL,
+  )
+  `)
 }
 
 /* 
