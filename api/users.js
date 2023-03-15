8a765d966c4fecab7c65127c4ac7d1a48bfcbd45
Charles Rooney <crooney36@gmail.com>
2023-03-15 14:56:13 -0400

    users api and error handler

--- api/users.js
+++ api/users.js
@@ -4,56 +4,36 @@ const usersRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { getUserByUsername, createUser, getUser } = require("../db");
 
-// usersRouter.use((req, res, next) => {
-//   console.log("A request is being made to /users");
-//   next();
-// });
+usersRouter.use((req, res, next) => {
+  console.log("A request is being made to /users");
+  next();
+});
 
 // POST /api/users/register
-
 usersRouter.post("/register", async (req, res, next) => {
-  // res.send({message: "HELLO"})
   try {
     const { username, password } = req.body;
-    if (!username || !password) {
-      next({
-        name: "UsernamePasswordError",
-        message: "Must enter a username and password",
-      });
-    }
-    console.log(password.length, "Password");
     const _user = await getUserByUsername(username);
     if (_user) {
-      res.status(401);
       next({
         name: "UserExistsError",
         message: `User ${username} is already taken.`,
       });
     } else if (password.length < 8) {
-      res.status(401);
-      console.log("PW");
       next({
         name: "PasswordTooShortError",
-        message: "Password must be at least 8 characters long",
+        message: "Password Too Short!",
       });
     } else {
-      console.log("creating user...");
       const user = await createUser({ username, password });
-      if (!user) {
-        next({
-          name: "CreatingUserError",
-          message: "Could not create user",
-        });
-      } else {
-        const token = jwt.sign(
-          { username: user.username, password: user.password },
-          process.env.JWT_SECRET,
-          {
-            expiresIn: "1w",
-          }
-        );
-        res.send({ message: "thank you for signing up", token, user });
-      }
+      const token = jwt.sign(
+        { username: user.username, password: user.password },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1w",
+        }
+      );
+      res.send({ message: "thank you for signing up", token, user });
     }
   } catch ({ name, message }) {
     next({ name, message });
@@ -64,6 +44,12 @@ usersRouter.post("/register", async (req, res, next) => {
 usersRouter.post("/login", async (req, res, next) => {
   try {
     const { username, password } = req.body;
+    if (!username || !password) {
+      next({
+        name: "loginError",
+        message: "Please enter username and password",
+      });
+    }
     const user = await getUser({ username, password });
     if (user) {
       const token = jwt.sign(
--- tests/api/users.spec.js
+++ tests/api/users.spec.js
@@ -134,7 +134,7 @@ describe("/api/users", () => {
   });
 
   describe("POST /api/users/login", () => {
-    xit("Logs in the user. Requires username and password, and verifies that hashed login password matches the saved hashed password.", async () => {
+    it("Logs in the user. Requires username and password, and verifies that hashed login password matches the saved hashed password.", async () => {
       // Create some fake user data
       const userData = {
         username: faker.internet.userName(),
@@ -156,7 +156,7 @@ describe("/api/users", () => {
       );
     });
 
-    xit("Logs in the user and returns the user back to us", async () => {
+    it("Logs in the user and returns the user back to us", async () => {
       // Create some fake user data
       const userData = {
         username: faker.internet.userName(),
@@ -177,7 +177,7 @@ describe("/api/users", () => {
       });
     });
 
-    xit("Returns a JSON Web Token. Stores the id and username in the token.", async () => {
+    it("Returns a JSON Web Token. Stores the id and username in the token.", async () => {
       const userData = {
         username: faker.internet.userName(),
         password: faker.internet.password(),
@@ -202,7 +202,7 @@ describe("/api/users", () => {
   });
 
   describe("GET /api/users/me", () => {
-    xit("sends back users data if valid token is supplied in header", async () => {
+    it("sends back users data if valid token is supplied in header", async () => {
       const { fakeUser, token } = await createFakeUserWithToken();
 
       const response = await request(app)
@@ -214,7 +214,7 @@ describe("/api/users", () => {
       expect(response.body).toEqual(objectContaining(fakeUser));
     });
 
-    xit("rejects requests with no valid token", async () => {
+    it("rejects requests with no valid token", async () => {
       const response = await request(app).get("/api/users/me");
 
       expect(response.status).toBe(401);
@@ -224,7 +224,7 @@ describe("/api/users", () => {
   });
 
   describe("GET /api/users/:username/routines", () => {
-    xit("Gets a list of public routines for a particular user.", async () => {
+    it("Gets a list of public routines for a particular user.", async () => {
       // Create a fake user with a bunch of routines associated
       const { fakeUser, token } = await createFakeUserWithRoutinesAndActivities(
         "Greg"
@@ -244,7 +244,7 @@ describe("/api/users", () => {
       expect(response.body).toEqual([...routinesFromDB]);
     });
 
-    xit("gets a list of all routines for the logged in user", async () => {
+    it("gets a list of all routines for the logged in user", async () => {
       const { fakeUser, token } = await createFakeUserWithRoutinesAndActivities(
         "Angela"
       );
