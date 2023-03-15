b6dffc10b41f35569382548550ac8b26b4c5426a
victor <vvazquez307@gmail.com>
2023-03-15 10:06:14 -0500

    server

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const router = express.Router();
+const activitiesRouter = express.Router();
+const { getAllActivities } = require('../db')
+
+activitiesRouter.use((req, res, next) => {
+    console.log("A request is being made to /activities");
+  
+    next();
+});
+
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
-
+activitiesRouter.get('/', async (req, res) => {
+    try {
+        const allActivities = await getAllActivities();
+        console.log(allActivities," /////////////////////////////")
+    
+        res.send(allActivities)
+    } catch (error) {
+        throw error;
+    }
+    
+})
 // POST /api/activities
 
 // PATCH /api/activities/:activityId
 
-module.exports = router;
+module.exports = activitiesRouter;
