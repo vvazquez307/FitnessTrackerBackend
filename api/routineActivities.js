785b48d22fcb6d1ad83fee1478a999271ad44511
victor <vvazquez307@gmail.com>
2023-03-16 14:30:37 -0500

    R_A

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const router = express.Router();
+const R_A_Router = express.Router();
+const {} = require('../db')
 
+R_A_Router.use((req, res, next) => {
+    console.log("A request is being made to /`routine_activities`");
+  
+    next();
+});
 // PATCH /api/routine_activities/:routineActivityId
+R_A_Router.patch('/routine_activities/:routineActivityId', requireUser, async (req, res, next) => {
 
+})
 // DELETE /api/routine_activities/:routineActivityId
 
 module.exports = router;
